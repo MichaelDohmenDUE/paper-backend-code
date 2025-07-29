fd16174791b35f08c857848cdf3495db95b0572d
faoezg <fatih.oezgan@gmail.com>
2025-07-29 05:14:41 +0200

    Fixed wrong sync and epsilon-greedy, works now

--- DQN.py
+++ DQN.py
@@ -19,7 +19,7 @@ def epsilon_greedy(q_values: torch.tensor, epsilon: float) -> torch.tensor:
     max_idx = (q_values == max_q_value).to(torch.int64)
     greedy_action = random.choice(actions[max_idx == 1])
 
-    return random.choice(actions) if epsilon < random.random() else greedy_action
+    return random.choice(actions) if random.random() < epsilon else greedy_action
 
 
 class ReplayBuffer:
@@ -46,25 +46,22 @@ class MultiLayerPerceptron(nn.Module):
     def __init__(self, state_size: int, hidden_size: int, action_size: int, activation_func: Callable):
         super().__init__()
         self.fc1 = nn.Linear(state_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, hidden_size)
-        self.fc3 = nn.Linear(hidden_size, action_size)
+        self.fc2 = nn.Linear(hidden_size, action_size)
         self.activation_func = activation_func
 
     def forward(self, x):
         x = self.activation_func(self.fc1(x))
-        x = self.activation_func(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
 
 
 if __name__ == '__main__':
-
     # initialization
     env = gym.make("CartPole-v1")
 
-    behavior_policy = MultiLayerPerceptron(state_size=env.observation_space.shape[0], hidden_size=64,
+    behavior_policy = MultiLayerPerceptron(state_size=env.observation_space.shape[0], hidden_size=32,
                                            action_size=env.action_space.n, activation_func=nn.ReLU())
-    target_policy = MultiLayerPerceptron(state_size=env.observation_space.shape[0], hidden_size=64,
+    target_policy = MultiLayerPerceptron(state_size=env.observation_space.shape[0], hidden_size=32,
                                          action_size=env.action_space.n, activation_func=nn.ReLU())
     target_policy.load_state_dict(behavior_policy.state_dict())
 
@@ -73,21 +70,21 @@ if __name__ == '__main__':
     buffer = ReplayBuffer()
 
     gamma = 0.99
-    num_episodes = 500
+    num_episodes = 400
+    update_freq = 40
 
     # Outer training loop
+    total_steps = 0
     for episode in range(num_episodes):
         state, _ = env.reset()
         done = False
 
-        total_steps = 0
-        total_rewards = 0
+        episode_rewards = 0
         while not done:
 
             # Data collection
             q_values = behavior_policy(torch.tensor(state, dtype=torch.float32))
             action = epsilon_greedy(q_values=q_values, epsilon=0.2)
-
             next_state, reward, terminated, truncated, info = env.step(action.item())
             done = terminated or truncated
 
@@ -96,7 +93,7 @@ if __name__ == '__main__':
             state = next_state
 
             # Learning
-            batch_size = 128
+            batch_size = 64
             if len(buffer) > batch_size:
                 transitions = buffer.sample(batch_size)
                 states, actions, rewards, next_states, dones = zip(*transitions)
@@ -110,7 +107,6 @@ if __name__ == '__main__':
                 qsa_behavior = behavior_policy(states_tensor).gather(1, actions_tensor)  # ^y
                 qs_target = target_policy(next_states_tensor)  # batch_size x action_dim
                 qsa_target = torch.max(qs_target, dim=1).values.unsqueeze(-1).detach()
-
                 target = rewards_tensor + gamma * qsa_target * (1.0 - dones_tensor) # [~dones_tensor]  # y
                 optimizer.zero_grad()
                 loss = criterion(qsa_behavior, target)
@@ -118,9 +114,10 @@ if __name__ == '__main__':
                 optimizer.step()
 
             total_steps += 1
-            total_rewards += reward
+            episode_rewards += reward
             # synchronization
-            if total_steps % 100 == 0:
+            if total_steps % update_freq == 0:
                 target_policy.load_state_dict(behavior_policy.state_dict())
-        print(total_rewards)
+        if episode % 10 == 0:
+            print(f"Episode [{episode}] {episode_rewards}")
 
