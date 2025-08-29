ccf17d16a2d9fd462f1a920d8ba4709587457f3a
faoezg <fatih.oezgan@gmail.com>
2025-08-29 18:39:20 +0200

    Working implementation of DDPG on InvertedPendulum

---
+++ DDPG.py
@@ -0,0 +1,145 @@
+from argparse import ArgumentError
+
+import torch
+from torch import nn
+from torch.nn import functional as F
+import numpy as np
+import gymnasium as gym
+from typing import Any
+from utils import ReplayBuffer
+
+
+class Actor(nn.Module):
+    def __init__(self, observation_size: int, hidden_size: int, action_size: int, action_scale: float = 1.0):
+        super().__init__()
+        self.fc1 = nn.Linear(observation_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, action_size)
+        self.action_scale = torch.tensor(action_scale, dtype=torch.float32)
+
+    def forward(self, x) -> torch.Tensor:
+        x = F.relu(self.fc1(x))
+        x = F.tanh(self.fc2(x)) * self.action_scale
+        return x
+
+
+class Critic(nn.Module):
+    def __init__(self, observation_size: int, hidden_size: int, action_size: int, output_size: int):
+        super().__init__()
+        self.fc1 = nn.Linear(observation_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size + action_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x, action):
+        x = F.relu(self.fc1(x))
+        x = torch.cat((x, action), dim=-1)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+def synchronize(from_network: nn.Module, to_network: nn.Module, tau: float) -> None:
+    """
+    Synchronize parameters from one network to another using soft or hard updates.
+
+    Args:
+        from_network (nn.Module): The source network (e.g., behavior network).
+        to_network (nn.Module): The target network to be updated.
+        tau (float): Interpolation factor for the update.
+            - tau = 1.0 → hard update.
+            - 0 < tau < 1.0 → soft update.
+            - tau = 0.0 → no update.
+
+    Returns:
+        None
+
+    Raises:
+        AssertionError: If `tau` is not within [0.0, 1.0]
+    """
+
+    assert 0.0 <= tau <= 1.0, f"tau must be in [0, 1], got {tau}"
+
+    with torch.no_grad():
+        if tau == 1.0:
+            to_network.load_state_dict(from_network.state_dict())
+        else:
+            for from_param, to_param in zip(from_network.parameters(), to_network.parameters()):
+                to_param.copy_(tau * from_param + (1.0 - tau) * to_param)
+
+
+if __name__ == '__main__':
+    env = gym.make("InvertedPendulum-v5")
+    action_scale = env.action_space.high
+    #env = gym.make("Pendulum-v1")
+    observation_size = env.observation_space.shape[0]
+    hidden_size = 32
+    action_size = env.action_space.shape[0]
+    output_size = 1
+
+    num_episodes = 15000
+    batch_size = 128
+
+    gamma = 0.99
+    lr=5e-3
+
+    # Initialize and synchronize behavior and target networks
+    actor = Actor(observation_size, hidden_size, action_size, action_scale)
+    actor_target = Actor(observation_size, hidden_size, action_size, action_scale)
+    actor_optimizer = torch.optim.Adam(actor.parameters(), lr=lr)
+
+    critic = Critic(observation_size, hidden_size, action_size, output_size)
+    critic_target = Critic(observation_size, hidden_size, action_size, output_size)
+    critic_optimizer = torch.optim.Adam(critic.parameters(), lr=lr)
+
+    synchronize(actor, actor_target, 1.0)
+    synchronize(critic, critic_target, 1.0)
+
+    buffer = ReplayBuffer()
+
+    for episode in range(num_episodes):
+
+        state, _ = env.reset()
+        done = False
+        episode_reward = 0
+        actor_loss, critic_loss = None, None
+
+        while not done:
+            state_tensor = torch.tensor(state, dtype=torch.float32)
+            action = actor(state_tensor)
+            action += torch.normal(0, 0.1, size=action.shape)
+            next_state, reward, terminated, truncated, _ = env.step([action.item()])
+            done = terminated or truncated
+
+            transition = (state, action.item(), reward, next_state, done)
+            buffer.append(transition)
+
+            state = next_state
+            episode_reward += reward
+            if len(buffer) > batch_size:
+                batch = buffer.sample(batch_size)
+                states, actions, rewards, next_states, dones = zip(*batch)
+
+                states_tensor = torch.tensor(np.array(states), dtype=torch.float32)
+                actions_tensor = torch.tensor(np.array(actions), dtype=torch.float32).unsqueeze(-1)
+                rewards_tensor = torch.tensor(np.array(rewards), dtype=torch.float32).unsqueeze(-1)
+                next_states_tensor = torch.tensor(np.array(next_states), dtype=torch.float32)
+                dones_tensor = torch.tensor(np.array(dones), dtype=torch.float32).unsqueeze(-1)
+
+                targets = (rewards_tensor + (1.0 - dones_tensor) * gamma *
+                           critic_target(next_states_tensor, actor_target(next_states_tensor))).detach()
+
+                critic_optimizer.zero_grad()
+                critic_loss = F.mse_loss(critic(states_tensor, actions_tensor), targets)
+                critic_loss.backward()
+                critic_optimizer.step()
+
+                actor_optimizer.zero_grad()
+                actor_loss = -critic(states_tensor, actor(states_tensor)).mean()
+                actor_loss.backward()
+                actor_optimizer.step()
+
+                synchronize(actor, actor_target, 0.005)
+                synchronize(critic, critic_target, 0.005)
+
+        if episode % 10 == 9:
+            if actor_loss is not None and critic_loss is not None:
+                print(f"Episode: {episode+1}, Reward: {episode_reward}, actor_loss: {actor_loss.mean():.3f}, critic_loss: {critic_loss:.3f}" )
