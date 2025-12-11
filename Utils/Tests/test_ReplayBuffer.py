67780753b3ad16bd0e75c5f645bc0b688bf895b8
Michael <michael_dohmen@gmx.net>
2025-12-11 13:40:46 +0100

    Additional Test for ReplayBuffer

--- Utils/Tests/test_ReplayBuffer.py
+++ Utils/Tests/test_ReplayBuffer.py
@@ -1,5 +1,5 @@
 import unittest
-
+from typing import Any
 from backend.Utils.src.ReplayBuffer import ReplayBuffer
 
 class TestReplayBuffer(unittest.TestCase):
@@ -33,6 +33,22 @@ class TestReplayBuffer(unittest.TestCase):
         self.assertEqual(len(sample), 3)
         for item in sample:
             self.assertIn(item, self.buffer.buffer)
+    def sample_sequence(self, seq_len: int, batch_size: int) -> list[list[Any]]:
+        length_buffer = len(self.buffer)
+        if seq_len > length_buffer:
+            raise BufferError(f"Buffer (Size {length_buffer}) is not long enough to allow a {seq_len}")
+        indices = [random.randint(0, length_buffer - seq_len) for _ in range(batch_size)]
+        sequences = []
+        for idx in indices:
+            sequence = [self.buffer[idx + t] for t in range(seq_len)]
+            sequences.append(sequence)
+        return sequences
+
+    def test_sample_sequence(self):
+        self.buffer.extend([10, 20, 30, 40, 50])
+        values = self.buffer.sample_sequence(2, batch_size=2)
+        # Check length
+        self.assertEqual(len(values), 2)
 
     def test_choice(self):
         self.buffer.extend(["a", "b", "c", "d", "e"])
