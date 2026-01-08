4e89d1082106f1926345eb4a7e2815939631a8bb
Michael <michael_dohmen@gmx.net>
2026-01-08 11:20:58 +0100

    Fix ENviromentHandling in DQN after ENviromentHandlerChanges

--- DQN/src/DataCollectionProcessor.py
+++ DQN/src/DataCollectionProcessor.py
@@ -22,6 +22,7 @@ class DataCollectionProcessor:
         self.episode_count = 0
         self.episode_reward = 0
         self.total_steps = 0
+        self.episode_steps = 0
 
     def run(self) -> None:
         if self.done:
@@ -30,22 +31,20 @@ class DataCollectionProcessor:
 
             self.episode_count += 1
             self.episode_reward = 0.0
-
             self.state = self.env.reset()
             self.done = False
+            self.episode_steps = 0
         with torch.no_grad():
             state_tensor = torch.tensor(self.state, dtype=torch.float32, device=self.device).unsqueeze(0)
-            q_values = self.policy(state_tensor)
-            q_values = q_values.squeeze(0)
+            q_values = self.policy(state_tensor).squeeze(0)
         action = self.action_selector.select_action(q_values=q_values)
-        next_state, reward, done, done_bool = self.env.step(action.item(), self.total_steps)
+        next_state, reward, done, done_bool = self.env.step(action.item(), self.episode_steps)
         self.done = done
-
+        self.episode_steps += 1
         transition = self.transition_factory.create(state=self.state, action=action.item(), reward=reward,
                                                     next_state=next_state, done=self.done)
+
         self.buffer.append(transition)
         self.episode_reward += reward
-
         self.state = next_state
-
         self.total_steps += 1
