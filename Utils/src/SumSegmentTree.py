c009b5ea6905e86c1186dc6eba2994b53aca4fc9
Michael <michael_dohmen@gmx.net>
2026-01-20 13:06:11 +0100

    Take the SumSegmentTree.py implementation from CleanRL

---
+++ Utils/src/SumSegmentTree.py
@@ -0,0 +1,55 @@
+# Taken from https://github.com/vwxyzjn/cleanrl/blob/004f8a086a892a2a180f4dd332b90d83a968aa7a/cleanrl/rainbow_atari.py
+# which adapted from: https://github.com/openai/baselines/blob/master/baselines/common/segment_tree.py
+import numpy as np
+
+class SumSegmentTree:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.tree_size = 2 * capacity - 1
+        self.tree = np.zeros(self.tree_size, dtype=np.float32)
+
+    def _propagate(self, idx):
+        parent = (idx - 1) // 2
+        while parent >= 0:
+            self.tree[parent] = self.tree[parent * 2 + 1] + self.tree[parent * 2 + 2]
+            parent = (parent - 1) // 2
+
+    def update(self, idx, value):
+        tree_idx = idx + self.capacity - 1
+        self.tree[tree_idx] = value
+        self._propagate(tree_idx)
+
+    def total(self):
+        return self.tree[0]
+
+    def retrieve(self, value):
+        idx = 0
+        while idx * 2 + 1 < self.tree_size:
+            left = idx * 2 + 1
+            right = left + 1
+            if value <= self.tree[left]:
+                idx = left
+            else:
+                value -= self.tree[left]
+                idx = right
+        return idx - (self.capacity - 1)
+
+class MinSegmentTree:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.tree_size = 2 * capacity - 1
+        self.tree = np.full(self.tree_size, float("inf"), dtype=np.float32)
+
+    def _propagate(self, idx):
+        parent = (idx - 1) // 2
+        while parent >= 0:
+            self.tree[parent] = min(self.tree[parent * 2 + 1], self.tree[parent * 2 + 2])
+            parent = (parent - 1) // 2
+
+    def update(self, idx, value):
+        tree_idx = idx + self.capacity - 1
+        self.tree[tree_idx] = value
+        self._propagate(tree_idx)
+
+    def min(self):
+        return self.tree[0]
