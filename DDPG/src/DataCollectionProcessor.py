1164f2f815e23e8980fcaa1300e037c5594295f6
Michael <michael_dohmen@gmx.net>
2026-02-10 12:56:06 +0100

    Some logging so I know it trains

--- DDPG/src/DataCollectionProcessor.py
+++ DDPG/src/DataCollectionProcessor.py
@@ -18,6 +18,7 @@ class DataCollectionProcessor:
         self.state = self.env.reset()
         self.done = False
         self.episode_timesteps = 0
+        self.episode_idx =0
 
     def run(self):
         state_tensor = torch.as_tensor(self.state, dtype=torch.float32, device=self.device)
@@ -44,7 +45,9 @@ class DataCollectionProcessor:
         self.done = done_bool
 
         if done:
-            print(f" — Reward: {self.episode_reward:.2f}")
+            self.episode_idx += 1
+            if self.episode_idx % 20 == 0:
+                print(f" — Reward: {self.episode_reward:.2f}")
             self.state = self.env.reset()
             self.episode_reward = 0
             self.episode_timesteps = 0
