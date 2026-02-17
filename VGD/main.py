0db8e5048c08e864179e2714e3daa3d6163d4497
MichaelDohmenDUE <michael.dohmen@stud.uni-due.de>
2026-02-17 13:41:44 +0100

    Vgd refactor (#8)
    * Remove baseline/advantage
    
    * Extracted Actionlogic into Handler
    
    * Extract DataCollector
    
    * Rename training function to run
    
    * Fix a typo
    
    ---------
    
    Co-authored-by: Michael <michael_dohmen@gmx.net>

--- VGD/main.py
+++ VGD/main.py
@@ -1,7 +1,11 @@
+from backend.Utils.src.BatchTransitioner import TransitionFactory, TransitionSpec
+from backend.Utils.src.ReplayBuffer import ReplayBuffer
+from backend.VGD.src.ActionHandler import ActionHandler
 import torch
 
 from backend.CommonModels.src.Policy_VPG import PolicyVPG
 from backend.Utils.src.EnviromentHandler import EnvironmentHandler
+from backend.VGD.src.DataCollector import DataCollectionProcessor
 from backend.VGD.src.TrainerVPG import VPGTrainer
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -21,14 +25,25 @@ def main():
     beta = 0.01
     gamma = 0.99
 
+    spec = TransitionSpec(["state", "action", "logp", "reward", "done"])
+    transition_factory = TransitionFactory(spec)
+    replay_buffer = ReplayBuffer(spec, max_buffer_size=500, batch_size=1)
+
     env_handler = EnvironmentHandler(env_name, seed=seed)
+
     observation_size, action_size, _ = env_handler.get_env_specs()
     policy = PolicyVPG(observation_size, action_size, hidden_dim=hidden_dim).to(device)
+
+    action_handler = ActionHandler(policy, device)
     optimizer = torch.optim.SGD(policy.parameters(), lr=learn_rate)
-    trainer = VPGTrainer(policy, env_handler, optimizer, beta, gamma, device=device)
+
+    data_collector = DataCollectionProcessor(env_handler, transition_factory, replay_buffer, action_handler)
+
+    trainer = VPGTrainer(policy, replay_buffer, action_handler, optimizer, beta, gamma, device=device)
 
     for episode in range(num_episodes):
-        ep_return, ep_length = trainer.train_episode()
+        data_collector.run()
+        ep_return, ep_length = trainer.run()
 
         if (episode + 1) % 50 == 0:
             print(f"Ep {episode + 1}: length {ep_length}, return {ep_return}")
--- VGD/src/TrainerVPG.py
+++ VGD/src/TrainerVPG.py
@@ -2,61 +2,47 @@ import numpy as np
 import torch
 
 from backend.CommonModels.src.Policy_VPG import PolicyVPG
-from backend.Utils.src import EnviromentHandler
+from backend.Utils.src import EnviromentHandler, ReplayBuffer
 from backend.Utils.src.utils import discounted_cumulative_reward
+from backend.VGD.src import ActionHandler
 
 
 class VPGTrainer:
     def __init__(self,
                  policy: PolicyVPG,
-                 enviroment_handler: EnviromentHandler,
+                 replay_buffer:ReplayBuffer.ReplayBuffer,
+                 action_handler: ActionHandler.ActionHandler,
                  optimizer,
                  beta: float = 0.01,
                  gamma: float = 0.99,
                  device: torch.device = torch.device("cpu")
                  ):
         self.policy = policy
-        self.enviroment_handler = enviroment_handler
+        self.action_handler = action_handler
+        self.replay_buffer = replay_buffer
         self.optimizer = optimizer
-        self.baseline_mean = 0.0
         self.beta = beta
         self.gamma = gamma
         self.device = device
 
-    def select_action(self, state):
-        state = torch.tensor(state, dtype=torch.float32).unsqueeze(0).to(self.device)
-        dist = self.policy.dist_categorical(state)
-        action_dist = dist.sample()
-        log_prob = dist.log_prob(action_dist).squeeze(0)
-        return int(action_dist.item()), log_prob
+    def run(self):
+        transitions = list(self.replay_buffer.buffer)
+        rewards = torch.tensor([t.reward for t in transitions], dtype=torch.float32, device=self.device)
+        dones = torch.tensor([t.done for t in transitions], dtype=torch.bool, device=self.device)
+        logps = torch.stack([t.logp for t in transitions]).to(self.device)
 
-    def train_episode(self):
-        obs = self.enviroment_handler.reset()
-        logps, rewards = [], []
-        done = False
-        t = 0
+        rewards_np = rewards.detach().cpu().numpy()
+        dones_np = dones.detach().cpu().numpy()
 
-        while not done:
-            action, log_prob = self.select_action(obs)
-            logps.append(log_prob)
-            obs, reward, done, _ = self.enviroment_handler.step(action, t)
-            rewards.append(reward)
-            t += 1
-
-        rewards = np.array(rewards, dtype=np.float64)
-        dones = np.zeros_like(rewards, dtype=np.bool_)
-        G = discounted_cumulative_reward(self.gamma, rewards, dones)
+        G = discounted_cumulative_reward(self.gamma, rewards_np, dones_np)
         G = torch.tensor(G, dtype=torch.float32).to(self.device)
 
-        b = torch.full_like(G, self.baseline_mean).to(self.device)
-        advantages = G - b
+        loss = -(logps * G).sum()
 
-        loss = -(torch.stack(logps) * advantages).sum().to(self.device)
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
 
         ep_return = float(sum(rewards))
-        self.baseline_mean = (1 - self.beta) * self.baseline_mean + self.beta * ep_return
 
         return ep_return, len(rewards)
