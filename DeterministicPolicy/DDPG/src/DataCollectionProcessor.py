a1f4d56cabbdee913ae3de28c863d50a4e1b2c3b
Michael <michael_dohmen@gmx.net>
2026-04-22 16:49:53 +0200

    Refactoring

--- DeterministicPolicy/DDPG/src/DataCollectionProcessor.py
+++ DeterministicPolicy/DDPG/src/DataCollectionProcessor.py
@@ -3,6 +3,7 @@ import torch
 from backend.Utils.src import ReplayBuffer, GlobalCounter
 from backend.Utils.src.BatchTransitioner import TransitionFactory
 from backend.Utils.src.EnviromentHandler import EnvironmentHandler
+from backend.Utils.src.NodeLib.NodeLibrary import reset_handler
 
 
 class DataCollectionProcessor:
@@ -16,7 +17,6 @@ class DataCollectionProcessor:
         self.global_counter = global_counter
         self.episode_reward = 0
         self.state = self.env.reset()
-        self.done = False
         self.episode_timesteps = 0
         self.episode_idx = 0
 
@@ -26,27 +26,28 @@ class DataCollectionProcessor:
         action_tensor = self.policy.select_action(state_tensor)
         action_np = action_tensor.cpu().numpy()
 
-        self.episode_timesteps += 1
-        next_state, reward, terminated, truncated, info = self.env.step_ddpg(action_np)
+        next_state, reward, terminated, truncated, _ = self.env.step_ddpg(action_np)
         self.episode_reward += reward
 
         transition = self.transition_factory.forward(state=self.state, action=action_np, reward=reward,
                                                      next_state=next_state, done=terminated)
         self.buffer.append(transition)
 
-        self.state = next_state
-        self.done = terminated
+        self.state = reset_handler(env=self.env, next_state=next_state, done=terminated or truncated)
+        #Reset PolicyNoise
+        if terminated or truncated:
+            self.policy.noise.reset()
+        #Logging
+        self.episode_timesteps += 1
         self.global_counter.set(self.global_counter.get() + 1)
         metrics = {}
         if terminated or truncated:
             metrics = {
                 "charts/episodic_return": self.episode_reward,
                 "charts/episodic_length": self.episode_timesteps,
-                "global_step": self.global_counter.get(),
+                "global_step": self.global_counter,
             }
-            self.state = self.env.reset()
             self.episode_reward = 0
             self.episode_timesteps = 0
             self.episode_idx += 1
-            self.policy.noise.reset()
         return metrics
