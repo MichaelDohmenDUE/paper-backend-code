93a0734bf3108aebfe9904ce65bb2508c5b2600a
Michael <michael_dohmen@gmx.net>
2026-04-26 19:38:14 +0200

    Clean Up Data Collection

--- StochasticPolicy/PPO/continuous/src/DataCollectionProcessor.py
+++ StochasticPolicy/PPO/continuous/src/DataCollectionProcessor.py
@@ -1,10 +1,7 @@
-import numpy as np
-import torch
-import wandb
-
 from backend.AbstractHandlers.AbstractActionHandler import AbstractActionHandler
 from backend.Utils.src.BatchTransitioner import TransitionFactory
 from backend.Utils.src.EnviromentHandler import EnvironmentHandler
+from backend.Utils.src.NodeLib.NodeLibrary import reset_handler
 from backend.Utils.src.ReplayBuffer import ReplayBuffer
 
 
@@ -18,44 +15,37 @@ class DataCollectionProcessor:
         self.rollout_size = rollout_size
         self.policy = action_handler
         self.total_steps = 0
-        self.last_state = None
+        self.state = self.env_handler.reset()
 
     def run(self):
-        metrics = {
-            "charts/episodic_return": 0,
-            "charts/episodic_length": 0,
-            "global_step": self.total_steps}
-        self.replay_buffer.buffer.clear()
         episode_timesteps = 0
         episodic_reward = 0
+        metrics = {}
 
-        if self.last_state is None:
-            self.last_state = self.env_handler.reset()
-        state = self.last_state
-        last_done = False
+        self.replay_buffer.buffer.clear()
+
         for _ in range(self.rollout_size):
-            self.total_steps += 1
-            episode_timesteps += 1
-            action, logp, value = self.policy.select_action(state)
+            action, logp, value = self.policy.select_action(self.state)
             next_state, reward, done, info = self.env_handler.step(action)
-            transition = self.transition_factory.forward(state=state, action=action, logp=logp, reward=reward,
-                                                         done=done, value=value, bootstrap_value=None)
+            transition = self.transition_factory.forward(state=self.state, action=action, logp=logp, reward=reward,
+                                                         done=done, value=value, bootstrap_value=None
+                                                         )
             self.replay_buffer.append(transition)
             episodic_reward += reward
-            state = next_state
-            last_done = done
+            episode_timesteps += 1
+            self.total_steps += 1
+            self.state = reset_handler(self.env_handler, next_state, done)
             if done:
                 metrics = {"charts/episodic_return": episodic_reward,
-                    "charts/episodic_length": episode_timesteps,
-                    "global_step": self.total_steps}
-                state = self.env_handler.reset()
+                           "charts/episodic_length": episode_timesteps,
+                           "global_step": self.total_steps
+                           }
                 episode_timesteps = 0
                 episodic_reward = 0
 
-        self.last_state = state
-        if last_done:
+        if done:
             last_value = 0.0
         else:
-            _, _, last_value = self.policy.select_action(state)
+            _, _, last_value = self.policy.select_action(self.state)
         self.replay_buffer.buffer[-1].bootstrap_value = last_value
         return metrics
