aa97461edf479788aa432f45003e6cb27315965b
Michael <michael_dohmen@gmx.net>
2026-04-29 11:55:01 +0200

    Logging Changes

--- DeterministicPolicy/DDPG/DDPG.py
+++ DeterministicPolicy/DDPG/DDPG.py
@@ -125,6 +125,6 @@ def main(seed, env_name):
     wandb.finish()
 if __name__ == "__main__":
     seeds = [1, 2, 3]
-    env_name = "HalfCheetah-v4"
+    env_name = "Walker2d-v4"
     for seed in seeds:
         main(seed, env_name)
