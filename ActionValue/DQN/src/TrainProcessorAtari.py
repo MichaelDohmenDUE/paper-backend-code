d0dee3cfc3b297a51c4ede07ce3c5960477603f7
Michael <michael_dohmen@gmx.net>
2026-04-30 21:58:58 +0200

    Add copy of Trainer to edit for Atari DQN

---
+++ ActionValue/DQN/src/TrainProcessorAtari.py
@@ -0,0 +1,67 @@
+import torch
+from torch import nn
+
+from backend.Utils.src.NodeLib.Node import Graph, Node, Signal
+from backend.Utils.src.NodeLib.NodeLibrary import bellman, mean_squared_error, detransition
+from backend.Utils.src.NodeLib.NodeLibrary import indexing, nl_max, optimizer_normalized
+from backend.Utils.src.ReplayBuffer import ReplayBuffer
+
+
+class TrainProcessor:
+    def __init__(self, buffer: ReplayBuffer, behavior_net: nn.Module, target_net: nn.Module,
+                 optimizer: torch.optim.Optimizer, gamma: float, max_norm: float, warmup_steps: int,
+                 device: torch.device):
+        self.buffer = buffer
+        self.behavior_net = behavior_net.to(device)
+        self.target_net = target_net.to(device)
+        self.optimizer = optimizer
+        self.gamma = gamma
+        self.device = device
+        self.max_norm = max_norm
+        self.warmup_steps = warmup_steps
+
+        self.context = {
+            "buffer": buffer,
+            "behavior_net": behavior_net.to(device),
+            "target_net": target_net.to(device),
+            "optimizer": optimizer,
+            "gamma": gamma,
+            "max_norm": max_norm,
+            "warmup_steps": warmup_steps,
+            "device": device,
+            "fields": buffer.spec.fields
+        }
+
+        nodes = [
+            Node("Sample", ["buffer", "warmup_steps"], ["batch"],
+                 function=lambda b, w: b.sample_batch() if len(b) >= w else Signal.NOSIGNAL),
+            Node("Detransition", ["fields", "batch", "device"], ["state", "action", "reward", "next_state", "done"],
+                 function=detransition),
+            Node("BehaviorForward", ["behavior_net", "state"], ["qs_b"],
+                 function=lambda net, s: net(s.squeeze(1))),
+            Node("QsaBehavior", ["qs_b", "action"], ["qsa_b"],
+                 function=lambda q, a: indexing(q, a).reshape(-1)),
+            Node("TargetForward", ["target_net", "next_state"], ["qs_t"],
+                 function=lambda net, ns: net(ns.squeeze(1)), no_grad=True),
+            Node("Max", ["qs_t"], ["max_q_t"],
+                 function=lambda qt: nl_max(qt).reshape(-1), no_grad=True),
+            Node("Bellman", ["max_q_t", "reward", "done", "gamma"], ["target_val"],
+                 function=bellman),
+            Node("Loss", ["qsa_b", "target_val"], ["loss"],
+                 function=mean_squared_error),
+            Node("Optimize", ["behavior_net", "optimizer", "loss", "max_norm"], ["_opt"],
+                 function=optimizer_normalized),
+
+            # Logging
+            Node("Metrics", ["loss", "qsa_b"], ["train_metrics"],
+                 function=lambda l, q: {
+                     "losses/td_loss": l.item(),
+                     "losses/q_values": q.mean().item()
+                 })
+        ]
+
+        self.graph = Graph(nodes, initial_keys=list(self.context.keys()))
+
+    def run(self):
+        self.graph.run(self.context)
+        return self.context.get("train_metrics", {})
