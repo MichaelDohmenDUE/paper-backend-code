acf82cf68cc624cd7494b40112ecba81c651033d
Michael <michael_dohmen@gmx.net>
2026-05-01 12:04:46 +0200

    Inint Version for Atari DDQN TRAiner by Copying regular DDQN Trainer

---
+++ ActionValue/DDQN/src/TrainProcessorAtari.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+from backend.Utils.src.NodeLib.Node import Node, Signal, Graph
+from backend.Utils.src.NodeLib.NodeLibrary import bellman, detransition, indexing, argmax, mean_squared_error, \
+    optimizer_normalized
+from backend.Utils.src.NodeLib.NodeLibrary import optimizer_update
+from backend.Utils.src.ReplayBuffer import ReplayBuffer
+
+
+class TrainProcessor:
+    def __init__(self, buffer: ReplayBuffer, behavior_net: nn.Module, target_net: nn.Module,
+                 optimizer: torch.optim.Optimizer, gamma: float, device: torch.device, warmup_steps=500):
+        self.buffer = buffer
+        self.behavior_net = behavior_net.to(device)
+        self.target_net = target_net.to(device)
+        self.optimizer = optimizer
+        self.gamma = gamma
+        self.warmup_steps = warmup_steps
+        self.device = device
+
+        self.context = {
+            "buffer": buffer,
+            "behavior_net": behavior_net.to(device),
+            "target_net": target_net.to(device),
+            "optimizer": optimizer,
+            "gamma": gamma,
+            "warmup_steps" : warmup_steps,
+            "device": device,
+            "fields": buffer.spec.fields
+        }
+
+        nodes = [
+            Node("Sample", ["buffer", "warmup_steps"], ["batch"],
+                 function=lambda b, w: b.sample_batch() if len(b) >= w else Signal.NOSIGNAL),
+
+            Node("Detransition", ["fields", "batch", "device"], ["state", "action", "reward", "next_state", "done"],
+                 function=detransition),
+
+            Node("FormatState", ["state"], ["s_sq"],
+                 function=lambda s: s.squeeze(1)),
+            Node("FormatNextState", ["next_state"], ["ns_sq"],
+                 function=lambda s: s.squeeze(1)),
+            Node("BehaviorForward", ["behavior_net", "s_sq"], ["qs_b"],
+                 function=lambda net, s: net(s)),
+            Node("QsaBehavior", ["qs_b", "action"], ["qsa_b"],
+                 function=lambda q, a: indexing(q, a)),
+            Node("BehaviorNextForward", ["behavior_net", "ns_sq"], ["next_qs_b"],
+                 function=lambda net, ns: net(ns), no_grad=True),
+            Node("SelectNextAction", ["next_qs_b"], ["next_actions"],
+                 function=lambda q: torch.argmax(q, dim=1).view(-1, 1), no_grad=True),
+            Node("TargetForward", ["target_net", "ns_sq"], ["qs_t"],
+                 function=lambda net, ns: net(ns), no_grad=True),
+            Node("QsaTarget", ["qs_t", "next_actions"], ["qsa_t"],
+                 function=lambda qt, a: indexing(qt, a.view(-1, 1)).reshape(-1), no_grad=True),
+            Node("Bellman", ["qsa_t", "reward", "done", "gamma"], ["target_val"],
+                 function=lambda q, r, d, g: bellman(target_Q=q, reward=r.reshape(-1), done=d.reshape(-1),
+                                                     discount_factor=g)),
+            Node("Loss", ["qsa_b", "target_val"], ["loss"],
+                 function=lambda q, t: mean_squared_error(q.reshape(-1), t.reshape(-1))),
+            Node("Optimize", ["optimizer", "loss"], ["_opt"],
+                 function=optimizer_update),
+
+            Node("Metrics", ["loss", "qsa_b"], ["train_metrics"],
+                 function=lambda l, q: {
+                     "losses/td_loss": l.item(),
+                     "losses/q_values": q.mean().item()
+                 })
+        ]
+
+        self.graph = Graph(nodes, initial_keys=list(self.context.keys()))
+
+    def run(self):
+        self.graph.run(self.context)
+        return self.context.get("train_metrics", {})
+
