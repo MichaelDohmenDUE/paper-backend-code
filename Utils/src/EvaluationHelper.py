67d294e4619d9b276c7220349d417a94e59534c9
Michael <michael_dohmen@gmx.net>
2026-05-03 01:35:39 +0200

    Fix Eval class for vecEnv

--- Utils/src/EvaluationHelper.py
+++ Utils/src/EvaluationHelper.py
@@ -1,25 +1,36 @@
+import numpy as np
 import torch
 
 
 def eval_trainer(trainer, env_handler, eval_episodes=5):
-    avg_reward = 0.0
     trainer.actor.eval()
-    for _ in range(eval_episodes):
-        state = env_handler.reset()
-        done = False
-        while not done:
-            state_t = torch.FloatTensor(state.reshape(1, -1)).to(trainer.device)
-            with torch.no_grad():
-                action = trainer.actor(state_t).cpu().numpy().flatten()
 
-            next_state, reward, done, _ = env_handler.step(action)
-            avg_reward += reward
-            state = next_state
-    avg_reward /= eval_episodes
+    episode_returns = []
+    current_episode_reward = 0.0
+
+    state = env_handler.reset()
+    if isinstance(state, tuple):
+        state = state[0]
+    while len(episode_returns) < eval_episodes:
+        state_t = torch.as_tensor(state, dtype=torch.float32, device=trainer.device)
+        if state_t.dim() == 1:
+            state_t = state_t.unsqueeze(0)
+        with torch.no_grad():
+            action = trainer.actor(state_t).cpu().numpy()
+
+        next_state, reward, done, _ = env_handler.step(action)
+        r = reward[0] if isinstance(reward, np.ndarray) else reward
+        current_episode_reward += r
+        if done[0]:
+            episode_returns.append(current_episode_reward)
+            current_episode_reward = 0.0
+
+        state = next_state
+
+    trainer.actor.train()
 
     metrics = {
-        "eval/eval_avg_reward": avg_reward,
+        "eval/eval_avg_reward": float(np.mean(episode_returns)),
     }
-    trainer.actor.train()
-    #print(f"Average Reward over {eval_episodes} episodes: {avg_reward:.3f}")
+
     return metrics
