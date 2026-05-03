4392f15afee170e5556ba5de96a4044ce6053fcd
Michael <michael_dohmen@gmx.net>
2026-05-03 23:24:42 +0200

    Fix imort

--- DeterministicPolicy/TD3/main.py
+++ DeterministicPolicy/TD3/main.py
@@ -144,6 +144,6 @@ def main(seed, env_name):
 
 if __name__ == "__main__":
     seeds = [0, 1, 2]
-    env_name = "HalfCheetah-v4"
+    env_name = "Hopper-v4"
     for current_seed in seeds:
         main(current_seed, env_name)
--- StochasticPolicy/PPO/continuous/src/DataCollectionProcessor.py
+++ StochasticPolicy/PPO/continuous/src/DataCollectionProcessor.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-from NodeLib.Node import PropsNode
-from NodeLib.NodeLibrary import to_tensor, to_numpy_array
+from backend.Utils.src.NodeLib.Node import PropsNode
+from backend.Utils.src.NodeLib.NodeLibrary import to_tensor, to_numpy_array
 from backend.StochasticPolicy.PPO.discrete.src.DataCollectorGraph import EpisodicMetricsNode
 from backend.Utils.src.NodeLib.Node import Node, Graph
 from backend.Utils.src.NodeLib.NodeLibrary import TransitionNode, BufferAppendingNode, BootStrappingNodeMujoco
 from backend.Utils.src.RolloutBuffer import RolloutBuffer
 
-from StochasticPolicy.PPO.discrete.src.DataCollectorGraphMujoco import merge_final_observations
+from backend.StochasticPolicy.PPO.discrete.src.DataCollectorGraphMujoco import merge_final_observations
 
 
 class DataCollectionProcessor:
